336f8aa3d1c00bfa905c078b4015bc048f3d6006
Kilnn <kilnn@qq.com>
2024-07-09 17:05:15 +0800

    opt version name

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "3.0.1-alpha1"
+        versionName = "1.2.4.9"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
