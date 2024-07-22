321ecc5d7acb945b2569f4e959086603da9ec605
Kilnn <kilnn@qq.com>
2024-07-22 11:29:09 +0800

    Optimize sample of NotificationConfig

--- sample/app/src/main/java/com/topstep/fitcloud/sample2/ui/device/config/NotificationConfigFragment.kt
+++ sample/app/src/main/java/com/topstep/fitcloud/sample2/ui/device/config/NotificationConfigFragment.kt
@@ -1,20 +1,28 @@
 package com.topstep.fitcloud.sample2.ui.device.config
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDialogFragment
-import com.github.kilnn.tool.widget.item.PreferenceItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.topstep.fitcloud.sample2.R
 import com.topstep.fitcloud.sample2.data.device.flowStateConnected
 import com.topstep.fitcloud.sample2.databinding.FragmentNotificationConfigBinding
+import com.topstep.fitcloud.sample2.databinding.ItemNotificationConfigBinding
 import com.topstep.fitcloud.sample2.di.Injector
 import com.topstep.fitcloud.sample2.ui.base.BaseFragment
-import com.topstep.fitcloud.sample2.utils.*
+import com.topstep.fitcloud.sample2.utils.PermissionHelper
+import com.topstep.fitcloud.sample2.utils.launchRepeatOnStarted
+import com.topstep.fitcloud.sample2.utils.launchWithLog
+import com.topstep.fitcloud.sample2.utils.viewLifecycle
 import com.topstep.fitcloud.sample2.utils.viewbinding.viewBinding
-import com.topstep.fitcloud.sdk.v2.model.config.FcDeviceInfo
+import com.topstep.fitcloud.sdk.apis.ability.base.FcNotificationAbility
 import com.topstep.fitcloud.sdk.v2.model.config.FcNotificationConfig
 import com.topstep.fitcloud.sdk.v2.model.config.toBuilder
 import com.topstep.fitcloud.sdk.v2.utils.notification.NotificationListenerServiceUtil
@@ -33,197 +41,216 @@ import kotlinx.coroutines.rx3.await
  * 1.[NotificationConfigFragment]
  * Display and modify
  */
-class NotificationConfigFragment : BaseFragment(R.layout.fragment_notification_config), CompoundButton.OnCheckedChangeListener {
+class NotificationConfigFragment : BaseFragment(R.layout.fragment_notification_config) {
 
     private val viewBind: FragmentNotificationConfigBinding by viewBinding()
 
     private val deviceManager = Injector.getDeviceManager()
     private val applicationScope = Injector.getApplicationScope()
 
-    private lateinit var config: FcNotificationConfig
+    private lateinit var adapter: NotificationConfigAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        config = deviceManager.configFeature.getNotificationConfig()
+        val config = deviceManager.configFeature.getNotificationConfig()
+        val items = deviceManager.fcSDK.notificationAbility.getSupportItems(getAllItemsName(requireContext()))
+        adapter = NotificationConfigAdapter(requireContext(), items, config)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewBind.recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        viewBind.recyclerView.adapter = adapter
+        adapter.listener = listener
+
         viewLifecycle.launchRepeatOnStarted {
             launch {
                 deviceManager.flowStateConnected().collect {
-                    viewBind.layoutContent.setAllChildEnabled(it)
-                    updateUI()
+                    adapter.isConnected = it
+                    adapter.isNLSEnabled = NotificationListenerServiceUtil.isEnabled(requireContext())
+                    adapter.notifyDataSetChanged()
                 }
             }
             launch {
                 deviceManager.configFeature.observerNotificationConfig().asFlow().collect {
-                    if (config != it) {
-                        config = it
-                        updateUI()
+                    if (adapter.config != it) {
+                        adapter.config = it
+                        adapter.notifyDataSetChanged()
                     }
                 }
             }
         }
-
-        viewBind.itemTelephony.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemSms.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemQq.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemWechat.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemFacebook.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemTwitter.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemInstagram.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemWhatsapp.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemLine.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemMessenger.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemKakaoTalk.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemSkype.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemEmail.getSwitchView().setOnCheckedChangeListener(this)
-        viewBind.itemOthers.getSwitchView().setOnCheckedChangeListener(this)
     }
 
-    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
-        if (buttonView.isPressed) {
-            when (buttonView) {
-                viewBind.itemTelephony.getSwitchView() -> {
+    private val listener = object : NotificationConfigAdapter.Listener {
+
+        private fun applyConfig(flag: Int, isChecked: Boolean) {
+            val config = adapter.config.toBuilder().setFlagEnabled(flag, isChecked).create()
+            applicationScope.launchWithLog {
+                deviceManager.configFeature.setNotificationConfig(config).await()
+            }
+            adapter.config = config
+        }
+
+        override fun onItemCheckedChange(item: NotificationNameFlag, buttonView: CompoundButton, isChecked: Boolean) {
+            when (item.flag) {
+                FcNotificationConfig.Flag.TELEPHONY -> {
                     if (isChecked) {
-                        PermissionHelper.requestTelephony(this) {
+                        PermissionHelper.requestTelephony(this@NotificationConfigFragment) {
                             if (PermissionHelper.hasReceiveTelephony(requireContext())) {
-                                changeFlags(true, FcNotificationConfig.Flag.TELEPHONY)
+                                applyConfig(FcNotificationConfig.Flag.TELEPHONY, true)
                             } else {
                                 buttonView.isChecked = false
                             }
                         }
                     } else {
-                        changeFlags(false, FcNotificationConfig.Flag.TELEPHONY)
+                        applyConfig(FcNotificationConfig.Flag.TELEPHONY, false)
                     }
                 }
-                viewBind.itemSms.getSwitchView() -> {
+                FcNotificationConfig.Flag.SMS -> {
                     if (isChecked) {
-                        PermissionHelper.requestSms(this) {
+                        PermissionHelper.requestSms(this@NotificationConfigFragment) {
                             if (PermissionHelper.hasReceiveSms(requireContext())) {
-                                changeFlags(true, FcNotificationConfig.Flag.SMS)
+                                applyConfig(FcNotificationConfig.Flag.SMS, true)
                             } else {
                                 buttonView.isChecked = false
                             }
                         }
                     } else {
-                        changeFlags(false, FcNotificationConfig.Flag.SMS)
+                        applyConfig(FcNotificationConfig.Flag.SMS, false)
                     }
                 }
-                viewBind.itemQq.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.QQ)
-                    }
-                }
-                viewBind.itemWechat.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.WECHAT)
-                    }
-                }
-                viewBind.itemFacebook.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.FACEBOOK)
-                    }
-                }
-                viewBind.itemTwitter.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.TWITTER)
-                    }
-                }
-                viewBind.itemInstagram.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.INSTAGRAM)
-                    }
-                }
-                viewBind.itemWhatsapp.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.WHATSAPP)
-                    }
-                }
-                viewBind.itemLine.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.LINE)
-                    }
-                }
-                viewBind.itemMessenger.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.FACEBOOK_MESSENGER)
-                    }
-                }
-                viewBind.itemKakaoTalk.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.KAKAO)
-                    }
-                }
-                viewBind.itemSkype.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.SKYPE)
-                    }
-                }
-                viewBind.itemEmail.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.EMAIL)
-                    }
-                }
-                viewBind.itemOthers.getSwitchView() -> {
-                    if (checkNotificationPermission(buttonView)) {
-                        changeFlags(isChecked, FcNotificationConfig.Flag.OTHERS_APP)
+                else -> {
+                    if (NotificationListenerServiceUtil.isEnabled(requireContext())) {
+                        applyConfig(item.flag, isChecked)
+                    } else {
+                        CaptureNotificationDialogFragment().show(childFragmentManager, null)
+                        buttonView.isChecked = false
                     }
                 }
             }
         }
     }
 
-    private fun FcNotificationConfig.saveConfig() {
-        applicationScope.launchWithLog {
-            deviceManager.configFeature.setNotificationConfig(this@saveConfig).await()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        adapter.listener = null
+    }
+
+    companion object {
+        private fun getAllItemsName(context: Context): Array<String> {
+            //All items name match of FcNotificationConfig.Flag
+            return arrayOf(
+                context.getString(R.string.ds_notification_telephony),
+                context.getString(R.string.ds_notification_sms),
+                context.getString(R.string.ds_notification_qq),
+                context.getString(R.string.ds_notification_wechat),
+                context.getString(R.string.ds_notification_facebook),
+                context.getString(R.string.ds_notification_twitter),
+                "LinkedIn",
+                context.getString(R.string.ds_notification_instagram),
+                "Pinterest",
+                context.getString(R.string.ds_notification_whatsapp),
+                context.getString(R.string.ds_notification_line),
+                "Facebook Messager",
+                context.getString(R.string.ds_notification_kakao_talk),
+                context.getString(R.string.ds_notification_skype),
+                context.getString(R.string.ds_notification_email),
+                "Telegram",
+                "Viber",
+                "Calendar",
+                "Snapchat",
+                "Hike",
+                "Youtube",
+                "Apple Music",
+                "Zoom",
+                "TikTok",
+                "Gmail",
+                "Outlook",
+                "Whatsapp Business",
+                "Fastrack",
+                "Titan",
+                "GPay",
+                "Amazon",
+                "Other Apps",
+            )
         }
-        this@NotificationConfigFragment.config = this
-        updateUI()
-    }
 
-    private fun changeFlags(isChecked: Boolean, flag: Int) {
-        config.toBuilder().setFlagEnabled(flag, isChecked).create().saveConfig()
-    }
-
-    private fun checkNotificationPermission(compoundButton: CompoundButton): Boolean {
-        if (NotificationListenerServiceUtil.isEnabled(requireContext())) return true
-        CaptureNotificationDialogFragment().show(childFragmentManager, null)
-        compoundButton.isChecked = false
-        return false
-    }
-
-    private fun updateUI() {
-        viewBind.itemTelephony.getSwitchView().isChecked = config.isFlagEnabled(FcNotificationConfig.Flag.TELEPHONY) && PermissionHelper.hasReceiveTelephony(requireContext())
-        viewBind.itemSms.getSwitchView().isChecked = config.isFlagEnabled(FcNotificationConfig.Flag.SMS) && PermissionHelper.hasReceiveSms(requireContext())
-
-        val isNLSEnabled = NotificationListenerServiceUtil.isEnabled(requireContext())
-        val deviceInfo = deviceManager.configFeature.getDeviceInfo()
-
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemQq, FcNotificationConfig.Flag.QQ)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemWechat, FcNotificationConfig.Flag.WECHAT)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemFacebook, FcNotificationConfig.Flag.FACEBOOK)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemTwitter, FcNotificationConfig.Flag.TWITTER)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemInstagram, FcNotificationConfig.Flag.INSTAGRAM)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemWhatsapp, FcNotificationConfig.Flag.WHATSAPP)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemLine, FcNotificationConfig.Flag.LINE)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemMessenger, FcNotificationConfig.Flag.FACEBOOK_MESSENGER)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemKakaoTalk, FcNotificationConfig.Flag.KAKAO)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemSkype, FcNotificationConfig.Flag.SKYPE)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemEmail, FcNotificationConfig.Flag.EMAIL)
-        updateThirdApp(deviceInfo, isNLSEnabled, viewBind.itemOthers, FcNotificationConfig.Flag.OTHERS_APP)
-    }
-
-    private fun updateThirdApp(deviceInfo: FcDeviceInfo, isNLSEnabled: Boolean, item: PreferenceItem, flag: Int) {
-        if (deviceInfo.isSupportNotification(flag)) {
-            item.visibility = View.VISIBLE
-            item.getSwitchView().isChecked = isNLSEnabled && config.isFlagEnabled(flag)
-        } else {
-            item.visibility = View.GONE
+        private fun FcNotificationAbility.getSupportItems(names: Array<String>): MutableList<NotificationNameFlag> {
+            val supports = ArrayList<NotificationNameFlag>()
+            for (i in names.indices) {
+                //Index equals FcNotificationConfig.Flag
+                if (isSupportNotification(i)) {
+                    supports.add(
+                        NotificationNameFlag(names[i], i)
+                    )
+                }
+            }
+            return supports
         }
     }
+
+}
+
+data class NotificationNameFlag(
+    val name: String,
+    @FcNotificationConfig.Flag val flag: Int,
+)
+
+class NotificationConfigAdapter(
+    private val context: Context,
+    private val sources: MutableList<NotificationNameFlag>,
+    var config: FcNotificationConfig,
+) : RecyclerView.Adapter<NotificationConfigAdapter.InnerViewHolder>() {
+
+    var isConnected = false
+    var isNLSEnabled = false
+    var listener: Listener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InnerViewHolder {
+        return InnerViewHolder(
+            ItemNotificationConfigBinding.inflate(LayoutInflater.from(parent.context))
+        )
+    }
+
+    override fun onBindViewHolder(holder: InnerViewHolder, position: Int) {
+        val item = sources[position]
+        val isFlagEnabled = config.isFlagEnabled(item.flag)
+
+        val titleView = holder.viewBind.item.getTitleView()
+        val switchView = holder.viewBind.item.getSwitchView()
+        titleView.text = item.name
+
+        when (item.flag) {
+            FcNotificationConfig.Flag.TELEPHONY -> {
+                switchView.isChecked = PermissionHelper.hasReceiveTelephony(context) && isFlagEnabled
+            }
+            FcNotificationConfig.Flag.SMS -> {
+                switchView.isChecked = PermissionHelper.hasReceiveSms(context) && isFlagEnabled
+            }
+            else -> {
+                switchView.isChecked = isNLSEnabled && isFlagEnabled
+            }
+        }
+        switchView.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (buttonView.isPressed) {
+                listener?.onItemCheckedChange(item, buttonView, isChecked)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return sources.size
+    }
+
+    interface Listener {
+        fun onItemCheckedChange(item: NotificationNameFlag, buttonView: CompoundButton, isChecked: Boolean)
+    }
+
+    class InnerViewHolder(
+        val viewBind: ItemNotificationConfigBinding,
+    ) : RecyclerView.ViewHolder(viewBind.root)
 
 }
 
